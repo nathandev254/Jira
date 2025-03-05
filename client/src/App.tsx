5258337d431432cfd3090a94ae58678d77352b32
Nathandev254 <warimwenathaniel@gmail.com>
2025-03-05 23:42:13 +0300

    refactored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
-      <p className="text-red-500">SUBMIT</p>
     </div>
   )
 }
