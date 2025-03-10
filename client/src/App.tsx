bbe58ee3839a2b1cb4f165e3fecaaebce726eab8
Nathandev254 <warimwenathaniel@gmail.com>
2025-03-10 22:15:26 +0300

    monday

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "./components/ui/input"
 
 
 function App() {
   return (
    <div className="flex gap-4">
+    <Input/>
     <Button variant="primary" >Primary</Button>
     <Button variant="secondary">Secondary</Button>
     <Button variant="destructive">Destructive</Button>
