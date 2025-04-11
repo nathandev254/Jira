988209e0b2e06942955d3485842408e51691e426
Nathandev254 <warimwenathaniel@gmail.com>
2025-04-11 22:40:07 +0300

    hello today

--- server/src/controllers/Taskscontroller.ts
+++ server/src/controllers/Taskscontroller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import { log } from "console";
 
 
 export const CreateTasks = (req: Request, res: Response): void => {
   const {email,} = req.body
+  console.log('hello')
 };
