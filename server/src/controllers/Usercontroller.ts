7950256230e1218bd064550e8163e7f0762edb41
Nathandev254 <warimwenathaniel@gmail.com>
2025-04-26 23:41:29 +0300

    weekend

--- server/src/controllers/Usercontroller.ts
+++ server/src/controllers/Usercontroller.ts
@@ -7,16 +7,20 @@ export const CreateUser = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-
   const data = req.body;
   try {
     const userdata = await prisma.user.create({ data });
 
     res.status(200).json("user created successfully");
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ error: "Failed to create the user" });
   }
+};
+
+export const GetUser = async (req: Request, res: Response): Promise<void> => {
+  const Allusers = await prisma.user.findMany();
+  res.status(200).json(Allusers);
 };
 
 export const GetUser = async (req: Request, res: Response): Promise<void> => {
