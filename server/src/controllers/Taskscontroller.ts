22185a3a411c128b24f389e7afad842f5b70dbfc
Nathandev254 <warimwenathaniel@gmail.com>
2025-05-07 22:12:34 +0300

    hello

--- server/src/controllers/Taskscontroller.ts
+++ server/src/controllers/Taskscontroller.ts
@@ -24,3 +24,31 @@ export const GetTasks = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json("failed to get the tasks");
   }
 };
+
+
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const CreateTasks = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const data = req.body;
+    const task = prisma.task.create({ data });
+    // console.log(data);
+    res.status(200).json({ message: "Task Created succefully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server error" });
+  }
+};
+
+export const GetTasks = async (req: Request, res: Response): Promise<void> => {
+  try {
+    res.status(200).json("All tasks");
+  } catch (error) {
+    res.status(500).json("failed to get the tasks");
+  }
+};
