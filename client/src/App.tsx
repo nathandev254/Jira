5ddd289e581c8643a53a3070b18cb96c4752a1f3
Nathandev254 <warimwenathaniel@gmail.com>
2025-05-19 14:05:21 +0300

    Happy monday

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@
 function App() {
   return (
    <div>
-    Home
+      <Sidebar/>
    </div>
   )
 }
--- client/src/Page.tsx
+++ client/src/Page.tsx
@@ -1,17 +1,9 @@
-import { Button } from "@/components/ui/button";
 import { Input } from "./components/ui/input";
 
 function Page() {
   return (
     <div>
       <Input />
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="teritary">teritary</Button>
     </div>
   );
 }
