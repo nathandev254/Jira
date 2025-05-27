af31d7e0618eeb5bb0c4aa2aae648c1f72f0d980
Nathandev254 <warimwenathaniel@gmail.com>
2025-05-27 22:42:30 +0300

    Today is tuesday

--- client/src/layouts/Sidebarlayout.tsx
+++ client/src/layouts/Sidebarlayout.tsx
@@ -1,11 +1,13 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/page/AppSidebar";
+import Navbar from "@/page/Navbar";
 import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
     <SidebarProvider>
       <AppSidebar />
+      <Navbar />
       <main>
         <SidebarTrigger />
         <Outlet />
---
+++ client/src/page/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@/components/ui/input";
+function Navbar() {
+  return <div >
+    <Input></Input>
+    <p>hekko this is the navbar</p>
+  </div>;
+}
+
+export default Navbar;
