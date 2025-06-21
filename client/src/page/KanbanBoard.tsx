5622c12079ed03b582e0c7bc40eae64c106f2835
Nathandev254 <warimwenathaniel@gmail.com>
2025-06-21 13:07:19 +0300

    Happy weekend

--- client/src/page/KanbanBoard.tsx
+++ client/src/page/KanbanBoard.tsx
@@ -1,3 +1,6 @@
+import { Badge } from "@/components/ui/badge";
+import { Calendar, MessageCircle } from "lucide-react";
+
 const taskBoard = [
   {
     title: "To Do",
@@ -73,16 +76,51 @@ const taskBoard = [
 
 function KanbanBoard() {
   return (
-    <div className=" p-2">
-      <h1 className="font-medium text-2xl">TaskBoard</h1>
-      <div className="flex gap-6 overflow-x-hidden ">
+    <div className=" p-2 w-full">
+      <h1 className="font-medium text-2xl mb-2">TaskBoard</h1>
+      <div className="flex gap-6 overflow-x-hidden  ">
         {taskBoard.map((column, index) => (
           <div className="w-full" key={index}>
-            <div className="flex justify-between items-center bg-green-500">
+            <div className="flex justify-between items-center mb-3">
               <h2>{column.title}</h2>
-              <span className="text-sm text-gray-500">
+              <span className="text-sm text-gray-800 h-5 w-5 rounded-full bg-gray-300 flex justify-center items-center">
                 {column.tasks.length}
               </span>
+            </div>
+            <div className="bg-gray-200 p-5 rounded-md flex flex-col gap-5">
+              {column.tasks.map((task, index) => (
+                <div key={index} className="bg-white p-4 rounded-md">
+                  <div className="flex justify-between mb-2">
+                    <Badge variant="destructive">{column.title}</Badge>
+                    <span className="text-sm">{task.priority}</span>
+                    <span className="h-5 w-5 rounded-full bg-gray-300 flex items-center justify-center">
+                      {task.assignee}
+                    </span>
+                  </div>
+                  <div>
+                    <h1 className="text-md font-bold mb-1">{task.title}</h1>
+                    <span className="h-3 w-3 rounded-full bg-red-600"></span>
+                    <span className="text-sm text-gray-400">
+                      {task.project}
+                    </span>
+                    <p className="text-gray-400 mt-1">{task.description}</p>
+                  </div>
+                  <div className="p-2 flex gap-2">
+                    {task.tags.map((tag, idx) => (
+                      <span key={idx}>{tag}</span>
+                    ))}
+                  </div>
+                  <div className="flex">
+                    <div>
+                      <Calendar/>
+                      {task.date}
+                    </div>
+                    <div>
+                      Messa
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         ))}
