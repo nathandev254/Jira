52b062ab981907b3de02a591951862af12fcf341
Nathandev254 <warimwenathaniel@gmail.com>
2025-06-27 23:03:29 +0300

    happy weekend

--- client/src/page/KanbanBoard.tsx
+++ client/src/page/KanbanBoard.tsx
@@ -92,6 +92,19 @@ function KanbanBoard() {
             </div>
           </div>
         ))}
+        {taskBoard.map((column, index) => (
+          <div className="w-full" key={index}>
+            <div className="flex justify-between items-center mb-3">
+              <h2>{column.title}</h2>
+              <span className="text-sm text-gray-800 h-5 w-5 rounded-full bg-gray-300 flex justify-center items-center">
+                {column.tasks.length}
+              </span>
+            </div>
+            <div className="bg-gray-200 p-5 rounded-md flex flex-col gap-5 h-full">
+              <Tasksboard tasks={column.tasks} title={column.title} />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
