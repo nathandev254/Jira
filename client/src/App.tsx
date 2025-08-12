5a7b7e4dbb7c1022fbecf2b60143feed73d86b70
Nathandev254 <warimwenathaniel@gmail.com>
2025-08-12 12:59:30 +0300

    working on Tanstack react query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,41 @@
-function App() {
-  return <div>Home</div>;
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+import Signin from "./page/Signin";
+import Authlayout from "./layouts/Authlayout";
+import Layout from "./layouts/Sidebarlayout";
+import Dashboard from "./page/Dashboard";
+import Tasks from "./page/Tasks";
+import Projects from "./page/Projects";
+import Teams from "./page/Teams";
+import Report from "./page/Report";
+import Settings from "./page/Settings";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Navigate to="dashboard" replace /> },
+      { path: "dashboard", element: <Dashboard /> },
+      { path: "tasks", element: <Tasks /> },
+      { path: "projects", element: <Projects /> },
+      { path: "Teams", element: <Teams /> },
+      { path: "report", element: <Report /> },
+      { path: "settings", element: <Settings /> },
+    ],
+  },
+  {
+    path: "",
+    element: <Authlayout />,
+    children: [{ path: "signin", element: <Signin /> }],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,45 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
 import "./index.css";
-import Signin from "./page/Signin.tsx";
-import Authlayout from "./layouts/Authlayout.tsx";
-import Layout from "./layouts/Sidebarlayout.tsx";
-import Dashboard from "./page/Dashboard.tsx";
-import Tasks from "./page/Tasks.tsx";
-import Projects from "./page/Projects.tsx";
-import Teams from "./page/Teams.tsx";
-import Report from "./page/Report.tsx";
-import Settings from "./page/Settings.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Navigate to="dashboard" replace /> },
-      { path: "dashboard", element: <Dashboard /> },
-      { path: "tasks", element: <Tasks /> },
-      { path: "projects", element: <Projects /> },
-      { path: "Teams", element: <Teams /> },
-      { path: "report", element: <Report /> },
-      { path: "settings", element: <Settings /> },
-    ],
-  },
-  {
-    path: "",
-    element: <Authlayout />,
-    children: [{ path: "signin", element: <Signin /> }],
-  },
-]);
+const Queryclient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={Queryclient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
